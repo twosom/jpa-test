34ca94a787b28761abcb2367943bbd3f9887d6ad
hope <hope@gravylab.co.kr>
2022-05-09 23:31:31 +0900

    Initial commit

---
+++ src/test/java/com/icloud/jpatest/JpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icloud.jpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
